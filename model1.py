62e46d976986cd1a72990bcc706e457003a6e3af
gix <gilbert.fritz@gmx.at>
2018-09-29 02:50:54 +0200

    modelled constraints: demand for month

--- model1.py
+++ model1.py
@@ -10,42 +10,43 @@ demandParam = pd.read_csv('demand_variables_1.csv', sep=';', index_col=['Month']
 # print(generatorsParam.loc['L','Max'])
 
 # DECISION VARIABLES
-genOutVarNames = []
-genOnOffVarNames = []
+p = []
 for gen in generatorsParam.index:
     for month in demandParam.index:
         # DECISION VARIABLE: p(i,t), generator Output per generator and month
-        genOutVarNames.append("p(" + gen + "," + str(month) + ")")
-        # DECISION VARIABLE: x(i,t), generator on or off per generator and month
-        genOnOffVarNames.append("x(" + gen + "," + str(month) + ")")
+        p.append( gen + "_" + str(month))
+
+i = []
+for month in demandParam.index:
+    i.append(str(month))
 
 # print(genOnOffVarNames);
 # print(genOutVarNames);
-genOutLpVar = pulp.LpVariable.dicts("genOut",
-                                    genOutVarNames,
-                                    cat='Integer')
+genOutLpVar = pulp.LpVariable.dicts("prod", p, cat='Continuous')
+importLpVar = pulp.LpVariable.dicts("import", i, cat='Continuous')
 
-genOnOffLpVar = pulp.LpVariable.dicts("genOnOff",
-                                      genOnOffVarNames,
-                                      cat='Binary')
-
-# MODEL OBJECTIVE
+#MODEL OBJECTIVE
 toSum = []
 for gen in generatorsParam.index:
     for month in demandParam.index:
-        idx = 'p(' + gen + ',' + str(month) + ')'
-        toSum.append(genOutLpVar[idx] * float(generatorsParam.loc[gen, 'Cost']))
-#print(toSum);
+        idxP = gen + "_" + str(month)
+        idxI = str(month)
+        toSum.append(genOutLpVar[idxP] * float(generatorsParam.loc[gen, 'VCost'])
+                                            + float(generatorsParam.loc[gen, 'VCost'])
+                                            + genOutLpVar[idxP] * float(generatorsParam.loc[gen, 'Emission']) * 20
+                                            + importLpVar[idxI] * float(demandParam.loc[month, 'ImportCost']));
+
 model = pulp.LpProblem("Cost minimising scheduling problem", pulp.LpMinimize)
 model += pulp.lpSum(toSum), "Output*Costs for each generator and each month"
 
 # MODEL CONSTRAINTS
 for month in demandParam.index:
     generatorsSum = []
+    idxI = str(month)
     for gen in generatorsParam.index:
-        idx = 'p(' + gen + ',' + str(month) + ')'
-        generatorsSum.append(genOutLpVar[idx])
-    model += pulp.lpSum(generatorsSum) - demandParam.loc[month] == 0, "Meet demand for month " + str(month)
+        idxP = gen + "_" + str(month)
+        generatorsSum.append(genOutLpVar[idxP])
+    model += pulp.lpSum(generatorsSum) + importLpVar[idxI] - demandParam.loc[month] == 0, "Meet demand for month " + str(month)
 
 print(model);
 
